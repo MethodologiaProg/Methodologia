7b7525696785fcc556a45da0410872d90fd72ba7
Chris <krisimen@gmail.com>
2016-04-11 14:59:12 +0300

    create new account added

---
+++ src/Controler/AddUser.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controler;
+
+import Model.User;
+import View.Profile;
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Hrystos
+ */
+public class AddUser {
+
+    public AddUser(String email, String password, String name, String lastname) {
+        DataSource ds = ConnectToDatabase.getDatasource();
+        Connection con = null;
+        Statement stmt = null;
+        System.out.println(email);
+        System.out.println(password);
+        System.out.println(name);
+        System.out.println(lastname);
+        try {
+            con = ds.getConnection();
+            stmt = con.createStatement();
+            stmt.executeUpdate("Insert into users (email, password, first_name, last_name) Values ('"+email+"','"+password+"','"+name+"','"+lastname+"')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+                try {
+                    if(stmt != null) stmt.close();
+                    if(con != null) con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
+        
+    }
+    
+}
